c94b8a70ef05b624c95d8c4184a314415960af59
James D <james@integratn.io>
2022-06-04 20:11:45 -0600

    added 1 more track and formatted

--- focusLofiPlaylist.tf
+++ focusLofiPlaylist.tf
@@ -9,53 +9,53 @@ resource "spotify_playlist" "focus_lofi" {
 locals {
   lofi_tracks = [
     {
-      artist = "Kid Kio",
-      song = "Somewhere New",
+      artist   = "Kid Kio",
+      song     = "Somewhere New",
       track_id = "4ruExpSbEiBnBa6uowGv3M",
     },
     {
-      artist = "Aso",
-      song = "Teakwood",
+      artist   = "Aso",
+      song     = "Teakwood",
       track_id = "1xW9riep92zpGZAW0HgmXJ",
     },
     {
-      artist = "Mood",
-      song = "Night Walk",
+      artist   = "Mood",
+      song     = "Night Walk",
       track_id = "4X9OLjeDCUTr39baLOSMlQ",
     },
     {
-      artist = "Ding M",
-      song = "Hollows",
+      artist   = "Ding M",
+      song     = "Hollows",
       track_id = "1LsBuFMHgiUlXYcH4PT2nv",
     },
     {
-      artist = "Dontcry",
-      song = "Drifting"
+      artist   = "Dontcry",
+      song     = "Drifting"
       track_id = "1mPjpQZdd5H4wvb4ZQ98Fm",
     },
     {
-      artist = "Rushee",
-      song = "Laidback Paradise",
+      artist   = "Rushee",
+      song     = "Laidback Paradise",
       track_id = "5vMQj2BQeVHxwnouYx2AfR",
     },
     {
-      artist = "Mo",
-      song = "Rana",
+      artist   = "Mo",
+      song     = "Rana",
       track_id = "1PKSLBf00OrjYd8bE8yRtl",
     },
     {
-      artist = "Ashley",
-      song = "Orikami",
+      artist   = "Ashley",
+      song     = "Orikami",
       track_id = "2ZpxtRMNvBkgQVZiCGIIub",
     },
     {
-      artist = "Kupla",
-      song = "Roots",
+      artist   = "Kupla",
+      song     = "Roots",
       track_id = "4ivra3rs51WBdEdlrnjwXb",
     },
     {
-      artist = "ThreeTuan",
-      song = "Clouds in the Sky",
+      artist   = "ThreeTuan",
+      song     = "Clouds in the Sky",
       track_id = "3BglIqMCvU6aPcJZLUqddw",
     },
   ]
